8dbf5ced46ffb88a731e34cdbb41d00be3c273d5
Frank Uzoka <Frankuzoka@hotmail.com>
2020-04-23 16:13:47 -0400

    All done!

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,7 +9,7 @@ public class App {
 
     }
 
-    static void logic(){
+    static void logic() {
 
         System.out.println("I'm thinking of a number between 1-100.  You have 7 guesses.");
 
@@ -18,45 +18,42 @@ public class App {
 
         System.out.print("First guess: ");
         int userInput = scan.nextInt();
-        int randomGuess = 1 ;
+        int randomGuess = 1;
         int tries = 7 - 1;
         int numberOfGuesses = 0;
         int numberOfGuessesTally = 2;
 
 
-        if( userInput == randomGuess ){
+        if (userInput == randomGuess) {
             System.out.println("You guessed it!  What are the odds?!?");
             System.exit(0);
 
-        }else{
-            if(userInput > randomGuess){
+        } else {
+            if (userInput > randomGuess) {
                 System.out.println("Sorry, that guess is too high.");
-            }
-            else  if(userInput > randomGuess){
+            } else if (userInput > randomGuess) {
                 System.out.println("Sorry, that guess is too low.");
             }
         }
 
-        while (tries != 0 && userInput != randomGuess){
-            System.out.print("Guess # "+numberOfGuessesTally+": " );
+        while (tries != 0 && userInput != randomGuess) {
+            System.out.print("Guess # " + numberOfGuessesTally + ": ");
             tries--;
             userInput = scan.nextInt();
 
-            if(tries == 0){
+            if (tries == 0) {
                 System.out.println("Sorry, you didn't guess it in 7 tries.  You lose.");
-            }else if(userInput > randomGuess){
+            } else if (userInput > randomGuess) {
                 System.out.println("Sorry, that guess is too high.");
-            }else if(userInput < randomGuess){
+                numberOfGuessesTally++;
+                numberOfGuesses++;
+            } else if (userInput < randomGuess) {
                 System.out.println("Sorry, that guess is too low.");
-            }else{
-                System.out.println("System.out.println(\"You guessed it!  What are the odds?!?\");");
+                numberOfGuessesTally++;
+                numberOfGuesses++;
+            } else {
+                System.out.println("You guessed it!  What are the odds?!?");
             }
-
-
-            numberOfGuessesTally++;
-            numberOfGuesses++;
-
         }
     }
-
 }
