689833a4120327cafa696d030614d17b1985ea04
Frank Uzoka <Frankuzoka@hotmail.com>
2020-04-23 15:03:24 -0400

    Set up variables

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
@@ -8,6 +11,16 @@ public class App {
 
     static void logic(){
 
+        System.out.println("I'm thinking of a number between 1-100.  You have 7 guesses.\n");
+
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+
+        int userInput = scan.nextInt();
+        int randomGuess = 1 + rand.nextInt(100);
+        int tries = 7;
+
+        while ()
     }
 
 }
